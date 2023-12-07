0f03b2b4850283719d41a30ef7479c9644c101c2
Soujannya Roy <soujannyaroy@gmail.com>
2023-12-08 01:39:49 +0530

    adding basic initial code

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import FirstPage from './src/FirstPage';
+
+function App() {
+  return (
+    <View>
+      <FirstPage />
+    </View>
+  );
+}
+
+export default App;
